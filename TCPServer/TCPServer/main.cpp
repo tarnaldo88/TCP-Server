d3d08d4e4bf49a6663e8270a3170fec8a61e5a79
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-01 23:15:44 -0700

    adding init of winsock and cleanup

--- TCPServer/TCPServer/main.cpp
+++ TCPServer/TCPServer/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <WS2tcpip.h>
+#include <winsock2.h>
 
 #pragma comment (lib, "ws3_32.lib")
 
 void main()
 {
 	//init Winsock
+    WSADATA wsaData;
+    int result;
+
+    // Initialize Winsock (version 2.2)
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        std::cerr << "WSAStartup failed with error: " << result << std::endl;
+        return 1;
+    }
 
 	//create socket
 
@@ -14,6 +24,6 @@ void main()
 
 
 	//close socket
-
+    WSACleanup();
 	//shutdown winsock
 }
