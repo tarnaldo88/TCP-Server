312660503fb068cb1e4cbc01f7fc555d881a6c0a
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-01 22:58:51 -0700

    initialize project

---
+++ TCPServer/TCPServer/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws3_32.lib")
+
+void main()
+{
+	//init Winsock
+
+	//create socket
+
+	//bind the socket to an ip address and port
+
+
+
+	//close socket
+
+	//shutdown winsock
+}
